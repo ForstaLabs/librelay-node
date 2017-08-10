0fcb0c1c7616a61b265d47886feb81a8d17c1c43
Justin Mayfield <tooker@gmail.com>
2017-08-10 14:04:35 -0600

    CCSM reg

--- src/account_manager.js
+++ src/account_manager.js
@@ -7,6 +7,7 @@ const EventEmitter = require('events');
 const ProvisioningCipher = require('./provisioning_cipher');
 const WebSocketResource = require('./websocket-resources');
 const api = require('./api');
+const fetch = require('node-fetch');
 const helpers = require('./helpers');
 const libsignal = require('libsignal');
 const node_crypto = require('crypto');
@@ -16,61 +17,71 @@ const storage = require('./storage');
 
 class AccountManager extends EventEmitter {
 
-    constructor(url, username, password, prekey_low_water=100,
-                prekey_high_water=500) {
+    constructor(url, username, password, prekey_low_water=20,
+                prekey_high_water=200) {
         super();
         this.server = new api.RelayServer(url, username, password);
         this.prekey_low_water = prekey_low_water;
         this.prekey_high_water = prekey_high_water;
     }
 
-    requestVoiceVerification(number) {
-        return this.server.requestVerificationVoice(number);
-    }
+    static async registerAccount(ccsm_url, ccsm_token, deviceName=null) {
+        let password = node_crypto.randomBytes(16).toString('base64');
+        password = password.substring(0, password.length - 2);
+        const signalingKey = node_crypto.randomBytes(32 + 20).toString('base64');
+        const registrationId = libsignal.KeyHelper.generateRegistrationId();
+        const request = {
+            password,
+            signalingKey,
+            registrationId,
+            deviceName,
+            userAgent: 'librelay-node'
+        };
 
-    requestSMSVerification(number) {
-        return this.server.requestVerificationSMS(number);
-    }
+        const r = await fetch(ccsm_url + '/v1/provision-proxy/', {
+            method: 'PUT',
+            headers: {
+                "Authorization": 'Token ' + ccsm_token,
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(request)
+        });
+        const resp = await r.json();
+        if (!r.ok) {
+            throw new Error(JSON.stringify(resp));
+        }
 
-    async registerSingleDevice(number, verificationCode) {
         const identityKeyPair = libsignal.KeyHelper.generateIdentityKeyPair();
-        await this.createAccount(number, verificationCode, identityKeyPair);
-        const keys = await this.generateKeys(this.prekey_high_water);
-        await this.server.registerKeys(keys);
+
+        await storage.protocol.clearSessionStore();
+        await storage.remove('number_id');
+        await storage.remove('device_name');
+        await storage.protocol.saveIdentity(resp.userId, identityKeyPair.pubKey);
+        await storage.protocol.setLocalIdentityKeyPair(identityKeyPair);
+        await storage.put_item('signaling_key', signalingKey);
+        await storage.put_item('password', password);
+        await storage.put_item('registrationId', registrationId);
+        await storage.put_item('serverUrl', resp.serverUrl);
+        await storage.user.setNumberAndDeviceId(resp.userId, resp.deviceId, deviceName);
+        await storage.put_item('regionCode', 'ZZ');
+        await storage.put_item('safety-numbers-approval', false);
+
+        const username = await storage.get_item('number_id');
+        const am = new this(resp.serverUrl, username, password);
+        const keys = await am.generateKeys(am.prekey_high_water);
+        await am.server.registerKeys(keys);
+        await storage.put_item('browserRegistrationDoneEver', '');
+        await storage.put_item('browserRegistrationDone', '');
+        return am;
     }
 
     async maybeRefreshPreKeys() {
         const available = await this.server.getMyKeys()
         if (available < this.prekey_low_water) {
-            // XXX We can't extend the existing list of keys so we have to go
-            // all the way to the high mark.
-            console.log('WARNING: Refilling prekey pool');
+            console.log('Refilling prekey pool');
             const keys = await this.generateKeys(this.prekey_high_water);
             await this.server.registerKeys(keys);
         }
-    }
-
-    async createAccount(number, verificationCode, identityKeyPair, deviceName) {
-        var signalingKey = node_crypto.randomBytes(32 + 20);
-        var password = node_crypto.randomBytes(16).toString('base64');
-        password = password.substring(0, password.length - 2);
-        var registrationId = libsignal.KeyHelper.generateRegistrationId();
-
-        const resp = await this.server.confirmCode(number, verificationCode,
-                                                   password, signalingKey,
-                                                   registrationId, deviceName);
-        await storage.protocol.clearSessionStore();
-        await storage.remove('number_id');
-        await storage.remove('device_name');
-        await storage.protocol.saveIdentity(number, identityKeyPair.pubKey);
-        await storage.protocol.setLocalIdentityKeyPair(identityKeyPair);
-        await storage.put_item('signaling_key', signalingKey.toString('base64'));
-        await storage.put_item('password', password);
-        await storage.put_item('registrationId', registrationId);
-        await storage.user.setNumberAndDeviceId(number, resp.deviceId || 1, deviceName);
-        await storage.put_item('regionCode', 'ZZ');
-        this.server.setUsername(await storage.get_item('number_id'));
-        this.server.setPassword(password);
     }
 
     async generateKeys(count) {
