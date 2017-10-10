a311c6a63e704dbab03d78d7b30059834df15e07
Justin Mayfield <tooker@gmail.com>
2017-10-10 13:30:18 -0600

    Update sendmessage example to include sync message.

--- examples/sendmessage.js
+++ examples/sendmessage.js
@@ -9,10 +9,12 @@ process.on('unhandledRejection', error => {
 (async function main() {
     const msgSender = await relay.MessageSender.factory();
     async function send(msg) {
+        const threadId = 'ae6a43d4-f0cd-41fc-9457-0d98fd11da36';
+        const now = Date.now();
         const bus = await msgSender.sendMessageToAddrs(["76399cf3-0898-4000-a565-0119fd1c2284"], [{
             version: 1,
             threadType: 'conversation',
-            threadId: 'ae6a43d4-f0cd-41fc-9457-0d98fd11da36',
+            threadId,
             messageType: 'content',
             messageId: uuid4(),
             userAgent: 'librelay',
@@ -28,11 +30,12 @@ process.on('unhandledRejection', error => {
             distribution: {
                 expression: '(<0ceeb1aa-fd9a-4df3-931d-864481574c54>+<cb6eb937-67e2-4cca-849a-d640b88d9eae>)',
             }
-        }], [], Date.now());
+        }], [], now);
         await new Promise((resolve, reject) => {
             bus.on('error', ev => reject(ev));
             bus.on('sent', ev => resolve(ev));
         });
+        await msgSender.sendSyncMessage(bus.messageBuffer, now, threadId);
     }
     const sendJobs = [];
     process.stdin.setEncoding('utf8');
